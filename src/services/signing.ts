be33c1a37ae70b9ee35248bce1199760343175e4
Christian Puchaicela <c@cmaginet.com>
2025-01-15 17:12:57 -0500

    'v0.4.0'

--- src/services/signing.ts
+++ src/services/signing.ts
@@ -68,7 +68,7 @@ export async function signXml(p12Data: ArrayBuffer, p12Password: string, xmlData
   const JAR_PATH = path.resolve(__dirname, "firma/firmaXadesBes.jar");
   const JAVA_CMD = "java";
 
-  console.log("xmlBase:", xmlBase);
+  console.log("p12Base64:", p12Base64);
   console.log("passwordBase64:", passwordBase64);
 
   return new Promise((resolve, reject) => {
